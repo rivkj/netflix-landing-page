38f767ece0310e7eedcbc086080753f0c8ad8178
rivkj <rivaskj07@gmail.com>
2024-02-28 17:09:11 -0500

    Edited JS file, js functionality

--- js/main.js
+++ js/main.js
@@ -3,14 +3,28 @@ const tabContentItems = document.querySelectorAll('.tab-content-item');
 
 // Select tab content item
 function selectItem(e) {
+  //remove all show and border classes
   removeBorder();
+  removeShow();
   //Add border to current tab
   this.classList.add('tab-border');
+  // Grab content item from DOM
+  const tabContentItem = document.querySelector(`#${this.id}-content`);
+  // Add show class
+  tabContentItem.classList.add('show');
 }
 
+//remove bottom borders from all items
 function removeBorder() {
-  tabItems.forEach(item => item.classList.remove('tab-border'));
+  tabItems.forEach(item => {item.classList.remove('tab-border');
+  });
+}
+
+function removeShow() {
+  tabContentItems.forEach(item => {item.classList.remove('show');
+  });
 }
 
 // Listen for tab click
-tabItems.forEach(item => item.addEventListener('click', selectItem));
+tabItems.forEach(item => {item.addEventListener('click', selectItem)
+});
